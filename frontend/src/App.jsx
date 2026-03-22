b05bc540da46d2f0a526aea9b4fc26b7239eed63
Panther-gehu <pranavbisht66@gmail.com>
2026-03-22 16:10:47 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
-import { Routes } from 'react-router-dom'
-import { Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+
 import Home from './pages/Home'
 import TextEditor from './pages/TextEditor'
+import Login from './pages/Login'
+import Signup from './pages/Signup'   
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,8 +12,10 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home/>} ></Route>
-        <Route path="/editor" element={<TextEditor/>}></Route>
+        <Route path="/" element={<Login />} />        
+        <Route path="/signup" element={<Signup />} />   {}
+        <Route path="/home" element={<Home />} />
+        <Route path="/editor" element={<TextEditor />} />
       </Routes>
     </>
   )
