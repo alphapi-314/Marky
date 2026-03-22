0a279511f40ccb8865f3778126ce28f64be6df3a
Panther-gehu <pranavbisht66@gmail.com>
2026-03-22 19:20:16 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,10 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import TextEditor from './pages/TextEditor'
 import Login from './pages/Login'
-import Signup from './pages/Signup'   
+import Signup from './pages/Signup'
+import ForgotPassword from './pages/ForgotPassword'
+import ResetPassword from './pages/ResetPassword'
+import VerifyOtp from './pages/VerifyOtp'   
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,8 +15,14 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login />} />        
-        <Route path="/signup" element={<Signup />} />   {}
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        
+        {}
+        <Route path="/verify-otp" element={<VerifyOtp />} />
+        
+        <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/home" element={<Home />} />
         <Route path="/editor" element={<TextEditor />} />
       </Routes>
